cc2d3c21069a069177a72bec2389ba7897a3010d
yiguo-17 <yiguo.qu@codeimmersives.com>
2020-10-06 19:16:07 -0400

    update text format.

--- addressList.js
+++ addressList.js
@@ -3,7 +3,13 @@ const readUser = ()=>{
     fs.readFile('addressList.json','utf8',(err,info)=>{
         if(err) throw err;
         else if(info.length ===0){console.log('There is no data in the file' )}
-        else{fs.writeFile('addressList.txt',info,(err)=>{
+        else{const obj = JSON.parse(info);
+            let str= '*****************\n* Address List: *\n*****************'
+            obj.map(person=>{
+                const { name, email, address: { street, suite, city, zipcode }, phone }=person;
+                str +=`\n\n${name}\nAddress: ${street}, ${suite}\n         ${city} ${zipcode}\nPhone:   ${phone}\nEmail:   ${email} `
+            })
+            fs.writeFile('addressList.txt',str,(err)=>{
             if(err) throw err;
             console.log('user info saved as plain text')
         })}
